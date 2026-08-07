eba7f5a8116587b9ddd93381284acc9e76f305b4
SherifBoshra <kizmo475@gmail.com>
2026-08-07 19:54:59 +0300

    mongodb schemes added

--- src/models/db_schemes/project.py
+++ src/models/db_schemes/project.py
@@ -1,13 +1,13 @@
-from pydantic import BaseModel , Field , validtor
+from pydantic import BaseModel , Field , validator
 from typing import Optional
 from bson.objectid import ObjectId
 
 class Project(BaseModel):
-    _id : Optional[ObjectId]
+    id: Optional[ObjectId] = Field(default=None, alias="_id")
     project_id : str = Field(..., min_length=1)
 
 
-    @validtor('project_id')
+    @validator('project_id')
     def validate_project_id(cls , value):
         if not value.isalnum():
             raise ValueError('project_id must be alphanumeric')
