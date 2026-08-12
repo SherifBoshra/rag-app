16e8a6b24f1a1dface5f3ed58999c0f30d782238
SherifBoshra <kizmo475@gmail.com>
2026-08-12 20:07:28 +0300

    made indecies for projects and chunks

--- src/routes/data.py
+++ src/routes/data.py
@@ -24,7 +24,7 @@ async def upload_data(request : Request ,project_id : str , file : UploadFile ,
                       app_settings : Settings = Depends(get_settings)):
 
 
-    project_model = ProjectModel(
+    project_model =await ProjectModel.create_instance(
         db_client=request.app.db_client
     )
 
@@ -83,7 +83,7 @@ async def process_endpoint(request : Request, project_id : str , process_request
     overlap_size = process_request.overlap_size
     do_reset = process_request.do_reset
 
-    project_model = ProjectModel(
+    project_model = await ProjectModel.create_instance(
         db_client=request.app.db_client
     )
     
@@ -124,7 +124,7 @@ async def process_endpoint(request : Request, project_id : str , process_request
         for i , chunk in enumerate(file_chunks)
     ]
 
-    chunk_model = ChunkModel(
+    chunk_model =await ChunkModel.create_instance(
         db_client=request.app.db_client
     )
     
